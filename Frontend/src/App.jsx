bd93c4d068cd3eb5b29f229c46043b1cf79ea6f6
Mansimishra444 <2k23.csai2312364@gmail.com>
2026-07-17 01:27:12 +0530

    done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Study from "./pages/Study";
 
 function App() {
-  
-
   return (
-    <>
-      
+    <BrowserRouter>
+      <Routes>
 
-    </>
-  )
+        <Route path="/" element={<Home />} />
+
+        <Route path="/study" element={<Study />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
